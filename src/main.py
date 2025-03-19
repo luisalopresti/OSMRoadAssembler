c29fc360173cf2c44de73fa55d09ab95d146be0b
luisalopresti <luisa.loprest@gmail.com>
2025-03-19 16:01:25 +0000

    save parquet

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from process_roads import *
 import argparse
 
-def main(place_name, tolerance, crs_latlon = 'EPSG:4326', crs_metric = 'EPSG:2157'):
+def main(place_name, simplify = False, tolerance = 100.0, crs_latlon = 'EPSG:4326', crs_metric = 'EPSG:2157'):
     # --------------------------------------------------------
     #                        Load data 
     # --------------------------------------------------------
@@ -32,21 +32,32 @@ def main(place_name, tolerance, crs_latlon = 'EPSG:4326', crs_metric = 'EPSG:215
     # --------------------------------------------------------
 
     process_road_names(edges)
-    final_edges = merge_segments(edges, crs_metric = 'EPSG:2157', crs_latlon = 'EPSG:4326', verbose = True)
+    final_edges = merge_segments(edges, crs_metric = crs_metric, crs_latlon = crs_latlon, verbose = True)
 
 
     # --------------------------------------------------------
     #           Apply Douglas-Peucker algorithm to
     #               final geometries (optional)
     # --------------------------------------------------------
-    final_edges['simplified_geometry'] = final_edges['geometry'].to_crs(crs_metric).simplify(tolerance, preserve_topology=True).to_crs(crs_latlon)
+    if simplify:
+        final_edges['simplified_geometry'] = final_edges['geometry'].to_crs(crs_metric).simplify(tolerance, preserve_topology=True).to_crs(crs_latlon)
 
+
+    # --------------------------------------------------------
+    #                  Save results as parquet
+    # --------------------------------------------------------    
+    # convert columns containing lists to datatype supported by parquet
+    for col in ['standardized_name', 'name', 'highway', 'osmid']:
+        final_edges[col] = final_edges[col].astype(str)
+    # save data as parquet
+    final_edges.to_parquet(f'../res/{place_name}.parquet')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process road network data for a given city.')
     parser.add_argument('--place_name', type=str, default='Dublin, Ireland', help='City name for road network extraction.')
-    parser.add_argument('--tolerance', type=float, default=100.0, help='Tolerance for Douglas-Peucker simplification in meters.')
+    # parser.add_argument('--simplify', type=bool, default=True, help='Whether to apply Douglas-Peucker simplification.')
+    # parser.add_argument('--tolerance', type=float, default=100.0, help='Tolerance for Douglas-Peucker simplification in meters.')
 
     args = parser.parse_args()
     main(args.place_name, args.tolerance)
